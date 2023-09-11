21f0815afe00e07a67b5db1c443baf6e56786d40
EyeceScream <jhb1085@gmail.com>
2023-09-11 14:52:50 -0500

    Added an About page.

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func main() {
 		log.Printf("parsing template: %v", err)
 		return
 	}
+
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
 	tpl, err = views.ParseTemplate(filepath.Join("templates", "faq.gohtml"))
@@ -44,6 +45,14 @@ func main() {
 	}
 
 	r.Get("/support", controllers.StaticHandler(tpl))
+
+	tpl, err = views.ParseTemplate(filepath.Join("templates", "about.gohtml"))
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		return
+	}
+
+	r.Get("/about", controllers.StaticHandler(tpl))
 
 	fmt.Println("Starting server on :3000")
 	http.ListenAndServe(":3000", r)
