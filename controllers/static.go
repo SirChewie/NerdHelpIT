f3a11fdb1167f82735fb2c6ac84509749655a0f1
EyeceScream <jhb1085@gmail.com>
2023-09-11 14:31:01 -0500

    Don't use snake case with imported funcs.

--- controllers/static.go
+++ controllers/static.go
@@ -6,7 +6,7 @@ import (
 	"github.com/SirChewie/NerdHelpIT/views"
 )
 
-func static_handler(tpl views.Template) http.HandlerFunc {
+func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
 	}
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"path/filepath"
 
+	"github.com/SirChewie/NerdHelpIT/controllers"
 	"github.com/SirChewie/NerdHelpIT/views"
 	"github.com/go-chi/chi/v5"
 )
@@ -19,14 +20,14 @@ func main() {
 		return
 	}
 
-	r.Get("/", controllers.static_handler(tpl))
+	r.Get("/", controllers.StaticHandler(tpl))
 
 	tpl, err = views.ParseTemplate(filepath.Join("templates", "contact.gohtml"))
 	if err != nil {
 		log.Printf("parsing template: %v", err)
 		return
 	}
-	r.Get("/contact", controllers.static_handler(tpl))
+	r.Get("/contact", controllers.StaticHandler(tpl))
 
 	tpl, err = views.ParseTemplate(filepath.Join("templates", "faq.gohtml"))
 	if err != nil {
@@ -34,7 +35,7 @@ func main() {
 		return
 	}
 
-	r.Get("/faq", controllers.static_handler(tpl))
+	r.Get("/faq", controllers.StaticHandler(tpl))
 
 	tpl, err = views.ParseTemplate(filepath.Join("templates", "support.gohtml"))
 	if err != nil {
@@ -42,7 +43,7 @@ func main() {
 		return
 	}
 
-	r.Get("/support", controllers.static_handler(tpl))
+	r.Get("/support", controllers.StaticHandler(tpl))
 
 	fmt.Println("Starting server on :3000")
 	http.ListenAndServe(":3000", r)
